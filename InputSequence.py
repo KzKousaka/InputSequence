22731a79df46151e8696230c77560b22e365a184
kope88 <kazuma1107@gmail.com>
2013-04-16 03:31:07 +0900

    update ver1.1

--- InputSequence.py
+++ InputSequence.py
@@ -1,53 +1,60 @@
 """
-InputSequenceCommand
-- Twitter: @kope_88
-
-This plug-in will output a sequence numbers in
-mulutiple caret position.
-
-ver 1.0 - 12.09.20
-"""
+    InputSequenceCommand
+    - Twitter: @kope_88
+    
+    This plug-in will output a sequence numbers in
+    mulutiple caret position.
+    
+    ver 1.1 - 2013.04.15
+    """
 
 import sublime, sublime_plugin
 
 class InputSequenceCommand(sublime_plugin.TextCommand):
-
+    
 	CH_CODE_SMALL_A = ord(u'a')
-	CH_CODE_SMALL_X = ord(u'x')
+	CH_CODE_SMALL_Z = ord(u'z')
 	CH_CODE_BIG_A = ord(u'A')
-	CH_CODE_BIG_X = ord(u'X')
+	CH_CODE_BIG_Z = ord(u'Z')
 	CH_CODE_0 = ord(u'0')
 	CH_CODE_9 = ord(u'9')
-
-
+    
+    
 	# return '0' or 'A' or 'a' or '(SPACE)' or 0
 	def check_char(self, ch) :
 		ch_num = ord(ch)
 		if self.CH_CODE_0         <= ch_num and self.CH_CODE_9       >= ch_num :
 			return self.CH_CODE_0
-		elif self.CH_CODE_BIG_A   <= ch_num and self.CH_CODE_BIG_X   >= ch_num :		# big character
+		elif self.CH_CODE_BIG_A   <= ch_num and self.CH_CODE_BIG_Z   >= ch_num :		# big character
 			return self.CH_CODE_BIG_A
-		elif self.CH_CODE_SMALL_A <= ch_num and self.CH_CODE_SMALL_X >= ch_num :		# small character
+		elif self.CH_CODE_SMALL_A <= ch_num and self.CH_CODE_SMALL_Z >= ch_num :		# small character
 			return self.CH_CODE_SMALL_A
 		
 		return 0
-
-
-
+    
+    
+    
 	def on_done(self, word):
-
-
+        
+		if word == "":
+			return;
+        
 		# check sequence type
 		charlist = list(word)
 		digits = len(charlist)
-
+        
 		# set decriment flag
 		decriment = False
+		overflow = False
 		if charlist[0] == u'-' :
 			decriment = True
 			charlist = charlist[1:digits]
 			digits -= 1
-
+		elif charlist[0] == u'$' :
+			overflow = True
+			charlist = charlist[1:digits]
+			digits -= 1
+        
 		# set digits type list
 		digitTypes = []
 		counter = []
@@ -55,54 +62,67 @@ class InputSequenceCommand(sublime_plugin.TextCommand):
 			ch_base = self.check_char(ch)
 			digitTypes.append(ch_base)
 			counter.append(ord(ch) - ch_base)
-
+        
 		edit = self.view.begin_edit();
-
+        
 		# get caret position
 		for region in self.view.sel():
-
+            
 			# set insert text
 			insert_str = u''.join(charlist)
-
+            
 			if region.empty():	# non select word
 				self.view.insert(self.edit, region.a, insert_str)
 			
 			else:				# select word
 				self.view.replace(edit, region, insert_str)
-
+            
 			# incriment word
 			for i in range(digits-1, -1 , -1) :
-
+                
 				carry = False
-
+                
 				# set ch_max
 				if digitTypes[i] == self.CH_CODE_0:
 					ch_max = self.CH_CODE_9 - self.CH_CODE_0
 				elif digitTypes[i] == self.CH_CODE_BIG_A:
-					ch_max = self.CH_CODE_BIG_X - self.CH_CODE_BIG_A
+					ch_max = self.CH_CODE_BIG_Z - self.CH_CODE_BIG_A
 				elif digitTypes[i] == self.CH_CODE_SMALL_A:
-					ch_max = self.CH_CODE_SMALL_X - self.CH_CODE_SMALL_A
-
-				print ch_max
+					ch_max = self.CH_CODE_SMALL_Z - self.CH_CODE_SMALL_A
+				else:
+					continue
+                
 				# incriment or decriment
 				counter[i] += 1 if not decriment else -1
-
+                
 				if counter[i] < 0 :
 					counter[i] = ch_max
 					carry = True
 				elif counter[i] > ch_max :
 					counter[i] = 0
 					carry = True
-
+                
 				charlist[i] =  chr(digitTypes[i] + counter[i])
-
+                
+				#Carry decision
 				if not carry:
 					break
-
+                
+                
+				#Add judgment of digits
+				if i == 0 and overflow:
+					digitTypes.insert(0, digitTypes[0] if digitTypes[0]!=0 else self.CH_CODE_0);
+					counter.insert(0, 1 if digitTypes[0] == self.CH_CODE_0 else 0);
+					charlist.insert(0,chr(digitTypes[0]+counter[0]))
+					digits+=1
+					break
+        
+        
+        
 		self.view.end_edit(edit)
-
-
-
+    
+    
+    
 	def run(self, edit):
 		self.edit = edit
-		self.view.window().show_input_panel('Sequence Type (alphabet or number):', '00', self.on_done, None,None)
+		self.view.window().show_input_panel('Sequence Type (alphabet or number):', '$0', self.on_done, None,None)
--- README.md
+++ README.md
@@ -1,31 +1,41 @@
 #InputSequence
 
-「Sublime Text 2」にインストールしてつかうプラグイン  
+「Sublime Text 2」にインストールして使用するプラグインです。
 Package Controlには追加していませんので、独自にインストールする必要があります。
 
 ## インストール
 Package Control から Add Repository を実行。  
 ウィンドウ下部にリポジトリパスの入力エリアが出現するので、下記を入力。  
 
-    https://github.com/wakuworks/InputSequence.git
+    https://github.com/kope88/InputSequence.git
 
 次に Install Package を実行し、InputSequence をインストール。
 
 ## アンインストール
 Package Control から Remove Package を実行し、InputSequence をアンインストール。  
 次にメニューから、`Preferences->Package Settings->Package Control->Settings - User`を開き、  
-repositories から `https://github.com/wakuworks/InputSequence.git` の一行を削除。
+repositories から `https://github.com/kope88/InputSequence.git` の一行を削除。
 
 ## 使い方
 `command+d`(Mac)などで、複数キャレットを表示させた状態にしておきます。
 
-上記インストール方法では、`ctrl+shift+0`に機能が割り当てられており、入力すると`Sequence Type (alphabet or number):`コマンドバーが出現し、初期コマンドは`00`になっています。
-このまま`Enter`を押せば2ケタの連番がカーソル位置に入力されます。
+ショートカットキーは`ctrl+shift+0`が割り当てられており、入力すると`Sequence Type (alphabet or number):`コマンドバーが出現します（初期コマンドは`$0`です）
+このまま`Enter`を押せば0からの連番がカーソル位置に入力されます。
 
-ほかに、
+その他、コマンド指定方法
 
 `aa`(小文字アルファベット連番)
 `AAA`(大文字アルファベット連番)
 `-10`(10からの降順連番)
+`05`(5からの連番。99のつぎは00になります)
+`$a0`(a0, a1, ... a9, b0, b1, ... z9, aa0とケタ上がりします)
 
-の機能も備えています。
+
+## 更新情報
+2013.4.15 :ver 1.1
+・ケタ追加機能を追加
+・英文字の連番がA-Zであるべきところ、A-Xとなっていたので修正
+
+
+2012.09.20 :ver 1.0
+初期バージョン作成
